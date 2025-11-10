581a4fa4643ae3cac30c6983d02770f30b62ebca
kly <kly@kly2005.localdomain>
2025-11-10 11:55:43 -0800

    Add HUDF files, write out steps for obtaining HUDF files

---
+++ proj2.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+from astropy.io import fits
+import numpy as np
+# from astropy.wcs import wcs
+# from astropy.table import Table
+
+# Step 1: get the HUDF images in 3 filters.
+# The original press release image (https://science.nasa.gov/asset/hubble/hubble-ultra-deep-field-image-reveals-galaxies-galore/)
+# states that the ACS/WFC instrument took the HUDF images between Sep 2003 and Jan 2004.
+# NASA description says HUDF images come from proposal 9978: 
+# https://mast.stsci.edu/search/ui/#/hst/results?proposal_id=9978
+# We'll choose the following files:
+## J8M802020 - F606W (green --> BLUE), 81.0 min exptime, 2003-09-25 20:09:20 UT
+## J8M803020 - F775W (red --> GREEN), 81.0 min exptime, 2003-09-27 00:57:17 UT
+## J8M804020 - F814W (IR --> RED), 81.0 min exptime, 2003-09-27 18:10:20 UT
+# We'll get the calibrated files in DRC format
+# Each DRC-calibrated file has an HDUList containing the science image (0), and uncertainty stuff (1 and 2).
+# Read the HUDF paper for more info https://iopscience.iop.org/article/10.1086/507302/pdf
+
+# Step 2: 
