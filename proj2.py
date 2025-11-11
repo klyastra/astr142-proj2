5043d93364124c3cd1e7f7fefcf1f27afbc0858a
kly <kly@kly2005.localdomain>
2025-11-10 18:03:56 -0800

    Implement z_spec VizieR query & circle plot. Changed colors of circle colors & opa opacities

--- proj2.py
+++ proj2.py
@@ -87,7 +87,7 @@ z_phot_vizier = Vizier(columns=['RAJ2000', 'DEJ2000', 'Vmag', 'zb'],
            column_filters={"Vmag":"<23"}, row_limit=z_phot_rowlimit)
 z_phot_list = z_phot_vizier.get_catalogs("J/AJ/132/926/catalog")[0]  # 0th index is the actual contents of the tablelist
 # "zb" = photometric redshift, "RAJ2000" and "DEJ2000" both in degrees (float values)
-print(z_phot_list['RAJ2000'], z_phot_list['DEJ2000'])
+## print(z_phot_list['RAJ2000'], z_phot_list['DEJ2000'])
 
 # plot circle patches for each row from the VizieR table
 for i in range(len(z_phot_list['RAJ2000'])):
@@ -103,17 +103,44 @@ for i in range(len(z_phot_list['RAJ2000'])):
     # We have RA/Dec coords from VizieR. Convert RA/Dec into pixels according to our WCS object named "w".
     ra_px, dec_px = w.world_to_pixel(skycoord_object)
 
-    # plot the circle patch at our converted locations (colored yellow for z_phot)
+    # plot the circle patch at our converted locations (colored magenta for z_phot)
     z_phot_circle = patches.Circle((ra_px, dec_px),
-                    radius=80, edgecolor='#FFFF00', facecolor='none', lw=0.5, label="z_phot only")
+                    radius=80, edgecolor='#FF00FF', facecolor='none', lw=0.5, alpha=0.5, label="z_phot only")
     ax.add_patch(z_phot_circle)
 
 ##### Spectroscopic redshift catalog #####
+# ADS:  https://ui.adsabs.harvard.edu/abs/2017A%26A...608A...2I/abstract
+# VizieR:  https://vizier.cds.unistra.fr/viz-bin/VizieR-3?-source=J/A%2bA/608/A2/combined
+z_spec_rowlimit = -1
+# set up filters and limits before querying VizieR.
+z_spec_vizier = Vizier(columns=['RAJ2000', 'DEJ2000', 'F775W', 'zMuse'],
+           column_filters={"F775W":"<23"}, row_limit=z_spec_rowlimit)
+z_spec_list = z_spec_vizier.get_catalogs("J/A+A/608/A2/combined")[0]  # 0th index is the actual contents of the tablelist
+# "zMuse" = spectroscopic redshift, "RAJ2000" and "DEJ2000" both in degrees (float values)
+print(z_spec_list['RAJ2000'], z_spec_list['DEJ2000'])
 
+# plot circle patches for each row from the VizieR table
+for i in range(len(z_spec_list['RAJ2000'])):
+    ra_float = z_spec_list['RAJ2000'][i]
+    dec_float = z_spec_list['DEJ2000'][i]
+
+    # Convert float to RA/Dec with units
+    skycoord_object = SkyCoord(ra=ra_float * u.degree, dec=dec_float * u.degree, frame='icrs')
+    ## ra, dec = skycoord_object.ra, skycoord_object.dec
+    # Plot the circle patch.
+    # By default, the circle patch's (x,y) coordinate argument is in units of pixels.
+    # Recall that the image size in pixels is 4217 x 4243 (in x,y orientation)
+    # We have RA/Dec coords from VizieR. Convert RA/Dec into pixels according to our WCS object named "w".
+    ra_px, dec_px = w.world_to_pixel(skycoord_object)
+
+    # plot the circle patch at our converted locations (colored yellow for z_phot)
+    z_spec_circle = patches.Circle((ra_px, dec_px),
+                    radius=80, edgecolor='#00FF00', facecolor='none', lw=0.5, alpha=0.5, label="z_spec only")
+    ax.add_patch(z_spec_circle)
 
 # Step 4: Plot phot/spec redshift catalog sources onto RGB image
 
 
-plt.legend(handles=[z_phot_circle], fontsize=6)  # legend for patch colors
+plt.legend(handles=[z_phot_circle, z_spec_circle], fontsize=6)  # legend for patch colors
 plt.savefig("HUDF.pdf", dpi=400)  # saving only works before showing plot
 plt.show()
